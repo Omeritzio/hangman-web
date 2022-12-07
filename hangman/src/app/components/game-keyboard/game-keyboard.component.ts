171869a98c1ade91b09bfabaa808331ea6dbaf2b
Omeritzio <omershlomo441@gmail.com>
2022-12-07 19:07:41 +0200

    proect

--- hangman/src/app/components/game-keyboard/game-keyboard.component.ts
+++ hangman/src/app/components/game-keyboard/game-keyboard.component.ts
@@ -1,5 +1,5 @@
 import { ThisReceiver } from '@angular/compiler';
-import { Component, Input ,OnInit} from '@angular/core';
+import { Component, Input,Output ,OnInit ,EventEmitter, OnChanges, SimpleChanges} from '@angular/core';
 import KEY_CHARS from 'src/app/constants/keyCharacter';
 
 
@@ -13,8 +13,10 @@ interface IKey {
   templateUrl: './game-keyboard.component.html',
   styleUrls: ['./game-keyboard.component.scss']
 })
-export class GameKeyboardComponent implements OnInit {
+export class GameKeyboardComponent implements OnInit, OnChanges{
+  
   @Input() question ='';
+  @Output() keyPressed = new EventEmitter<string>();
   keys :IKey[]=[];
 
   constructor(){
@@ -32,7 +34,37 @@ export class GameKeyboardComponent implements OnInit {
 
   }
 
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes?.['question']?.currentValue &&
+      changes?.['question'].currentValue !== changes?.['question'].previousValue
+    ) {
+      this.addMissingKeys();
+    }
+  }
+
+  addMissingKeys(): void {
+    for (let i = 0; i < this.question.length; i++) {
+      const keyExists = this.keys.find((key) => {
+        return key.value.toLowerCase() === this.question[i].toLowerCase();
+      });
+      if (keyExists) {
+        continue;
+      }
+      const randomIndex = Math.floor(Math.random() * 1);
+      this.keys.splice(randomIndex, 0, {
+        value: this.question[i],
+        guessed: false,
+      });
+    }
+  }
+
   onKeyClick(key:IKey):void {
+    if(key.guessed){
+      return;
+    }
     key.guessed =true;
+    this.keyPressed.emit(key.value);
   }
 }
