e766e2cb125efabde2daabba654d41e32962fbc6
Omeritzio <omershlomo441@gmail.com>
2022-12-15 13:20:54 +0200

    game

--- hangman/src/app/app-routing.module.ts
+++ hangman/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Route, Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
 
-const routes: Routes = [
-  {path:'' , component:GameComponent},
-];
+// const routes: Routes = [
+//   {path:'' , component:GameComponent},
+// ];
 
+const routes: Routes = [
+  { path: '', loadChildren: () => import('src/app/components/game/game.component').then(m => m.GameComponent) }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
